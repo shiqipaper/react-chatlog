e2084e965a664abd3ab145e310d356782219a4b4
luqi <lq.xie.work@gmail.com>
2024-12-16 00:01:24 -0800

    complete wave1 and pass test wave1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import chatData from './data/messages.json';
+
 
 const App = () => {
+  const firstChat = chatData[0];
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
+        <h2></h2>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={firstChat.sender}
+          body={firstChat.body}
+          timeStamp={firstChat.timeStamp}
+          />
       </main>
     </div>
   );
